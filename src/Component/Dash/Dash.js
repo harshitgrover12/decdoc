ef0d7a06d26261df11cd9fd7a2ccacfbb70dd84c
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2020-07-08 20:29:15 +0530

    issue document route added

--- src/Component/Dash/Dash.js
+++ src/Component/Dash/Dash.js
@@ -1,114 +1,14 @@
 import React, { Component } from 'react'
 import Nav from '../nav/nav';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-const styles = theme => ({
-  root: {
-    width: 345,
-    marginTop:'200px',
-    
-    display:'inline-block'
-  },
-  media: {
-    height: 140,
-  },
-});
-
+import Options from './Options/Options';
  class Dash extends Component {
-    
-     handleIssue=(e)=>{
-        e.preventDefault();
-}
-      
     render() {
-       const {classes}=this.props;
         return (
             <div>
-                <Nav/>
-                <Card className={classes.root} style={{marginLeft:'90px'}}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="issue document"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-           Issue a document
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Issue the document to a user 
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-             <Card className={classes.root} style={{marginLeft:'25px'}}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="issue document"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-           Verify a document
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Verify the credibility of a document
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-           <Card className={classes.root} style={{marginLeft:'25px'}}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="issue document"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-           Upload a document
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Upload a personal document
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-                
+              <Nav/> 
+              <Options{...this.props}/> 
             </div>
         )
     }
 }
-export default withStyles(styles,{theme:true})(Dash);
+export default Dash;
