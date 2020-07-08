18921d7ab6719b8b71ee398d6fcb90461dcf81e6
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2020-07-08 18:26:02 +0530

    some change

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Home from './Component/Home/Home';
 import SignIn from './Component/signIn/SignIn';
 import FileUpload from './Component/fileUpload/fileUpload';
+
  class Routes extends Component {
     render() {
         return (
             <div>
-            <BrowserRouter>
+            <Switch>
                 <Route exact path='/' render={(props)=><Home{...this.props}/>}/>
                 <Route exact path='/signIn' render={(props)=><SignIn{...this.props}/>}/>
                 <Route exact path='/fileUpload' render={(props)=><FileUpload{...this.props}/>}/>
-            </BrowserRouter>
+                
+            </Switch>
                 
             </div>
         )
