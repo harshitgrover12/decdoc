6de301d0ca59e2bbe85f8d141bd7196f406b20d5
shivikasaggar <shivikasagar@gmail.com>
2020-07-10 04:25:02 +0800

    navbar isuue

--- src/Component/about/About.js
+++ src/Component/about/About.js
@@ -1,51 +1,11 @@
 import React, { Component } from 'react'
+import Nav from '../nav/nav';
 class About extends Component {
     render() {
 
         return (
             <div>
-            <header id="header" className="fixed-top ">
-                    <div className="container-fluid">
-                        <div className="row justify-content-center">
-                            <div className="col-xl-9 d-flex align-items-center">
-                                <h1 className="logo mr-auto"><a href="/">DEC-DOCS</a></h1>
-                                {/* Uncomment below if you prefer to use an image logo */}
-                                {/* <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>*/}
-                                <nav className="nav-menu d-none d-lg-block">
-                                    <ul>
-                                        <li className="active"><a href="index.html">Home</a></li>
-                                        <li><a href="#about">About Us</a></li>
-                                        <li><a href="#services">Features</a></li>
-
-                                        <li><a href="#features">How It Works?</a></li>
-                                        <li><a href="#faq">FAQ</a></li>
-                                        {/*<li className="drop-down"><a href>Drop Down</a>
-                      <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li className="drop-down"><a href="#">Deep Drop Down</a>
-                          <ul>
-                            <li><a href="#">Deep Drop Down 1</a></li>
-                            <li><a href="#">Deep Drop Down 2</a></li>
-                            <li><a href="#">Deep Drop Down 3</a></li>
-                            <li><a href="#">Deep Drop Down 4</a></li>
-                            <li><a href="#">Deep Drop Down 5</a></li>
-                          </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                      </ul>
-                    </li>*/}
-                                        <li><a href="#contact">Contact</a></li>
-                                        <li><a href="/signIn">SignIn/SignUp</a></li>
-                                    </ul>
-                                </nav>
-                                {/* .nav-menu */}
-
-                            </div>
-                        </div>
-                    </div>
-                </header>
+                <Nav />
             <div className="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
                 
 
