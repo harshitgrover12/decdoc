999f38e6583928d9d296ce842955eb762b0b580c
shivikasaggar <shivikasagar@gmail.com>
2020-07-27 01:35:40 +0530

    layout

--- src/Component/createUser/CreateUser.js
+++ src/Component/createUser/CreateUser.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import '../Dash/IssueDocument/issueDocument.css';
+import Nav from '../nav/nav';
  class CreateUser extends Component {
      handleChange=(e)=>{
          this.setState(
@@ -35,14 +37,52 @@ import axios from 'axios';
     render() {
         return (
             <div>
-                 <div className="row px-3"> <label className="mb-1">
+                <Nav />
+
+                <div className="containerdoc justify-content-center center " >
+                    <div class="row docpad">
+                        <div class="col-xl-12 col-xl-offset-3 center labels">
+                            <label className="mb-1 ">
+                                <h6 className="mb-0 text-md"> NAME</h6>
+                            </label>
+                            <input className="mb-4 " id="name" type="text" name="name" placeholder="name" ref={(input) => this.name = input} onChange={this.handleChange} /> </div>
+
+                        </div>
+                   
+                    <div class="row docpad">
+                        <div class="col-xl-12 col-xl-offset-3 center labels">
+                        <label className="mb-1 ">
+                            <h6 className="mb-0 text-md">SECRET KEY</h6>
+                            </label>
+                            <input className="mb-4 " id="secret" type="password" name="secret" placeholder="Enter Secret key" ref={(input) => this.secret = input} onChange={this.handleChange} /> </div>
+
+                    </div>
+
+                    <div class="row docpad">
+                        <div class="col-xl-12 col-xl-offset-3 center">
+                            <div class="btn-container">
+                                <button type="submit" class="btn btn-primary btn-lg" onClick={this.handleSubmit}>Add to Blockchain!</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div >
+
+
+
+         /*<div className="row px-3"> <label className="mb-1">
                     <h6 className="mb-0 text-sm"> Name</h6>
                   </label> <input className="mb-4" id="name" type="text" name="name" placeholder="name" ref={(input)=>this.name=input} onChange={this.handleChange}/> </div>
                 <div className="row px-3"> <label className="mb-1">
                     <h6 className="mb-0 text-sm">Secret key</h6>
                   </label> <input id="secret" type="password" name="secret" placeholder="Enter Secret key" ref={(input)=>this.secret=input} onChange={this.handleChange} /> </div>
                             <div className="row mb-3 px-3"> <button type="submit" className="btn btn-blue text-center" onClick={this.handleSubmit}>Add to Blockchain!</button> </div>
-            </div>
+           */
+
+
+
+                 
         )
     }
 }
