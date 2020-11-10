d6f6ac9f00786fc38a334ddeb159010e99d4096f
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2020-11-10 12:58:27 +0530

    issue agreement from senders perspective done

--- src/Component/Dash/SignContract/SignContract.js
+++ src/Component/Dash/SignContract/SignContract.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import Nav from '../../nav/nav';
+import axios from 'axios';
  class SignContract extends Component {
           state = {
     // Initially, no file is selected
     selectedFile: null,
+    
   };
     onFileChange = (event) => {
     // Update the state
@@ -40,6 +42,37 @@ import Nav from '../../nav/nav';
       );
     }
   };
+  onFileUpload=async(e)=>
+  {
+    const data = new FormData(); 
+    data.append('file', this.state.selectedFile);
+    let hash;
+    
+    let userId;
+    let id;
+    
+		await axios.post('https://mysterious-temple-37666.herokuapp.com/filehash',data).then(async(res)=>{hash=res.data.documentHash;
+    
+    await axios.post('https://mysterious-temple-37666.herokuapp.com/api/getuser',{
+      username:this.state.username
+    }).then(({data})=>{
+      console.log(data);
+      id=data.userData.id
+      console.log('reciever id is',data.userData.id);
+      
+
+    }).
+    catch((e)=>alert(e));
+    console.log(this.props.userdata.id)
+    await axios.post('https://mysterious-temple-37666.herokuapp.com/issueAgreement',{
+      private_key:this.state.private_key,
+      documentHash:hash,
+      receiver:id,
+      sender:this.props.userdata.id
+    }).then((res)=>console.log(res)).catch((e)=>console.log(e))
+  })
+  
+  }
     render() {
         return (
             <div>
